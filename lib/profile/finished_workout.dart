cf6ae34bdd1d392ee4f05612e9965aba111be6a4
TanishqBhadula <tanishqbhadula@gmail.com>
2026-07-05 23:57:25 +0530

    finished workout page initial

---
+++ lib/profile/finished_workout.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class FinishedWorkout extends StatefulWidget {
+  const FinishedWorkout({super.key});
+  State<FinishedWorkout> createState() {
+    return _FinishedWorkout();
+  }
+}
+
+class _FinishedWorkout extends State<FinishedWorkout> {
+  @override
+  Widget build(BuildContext context) {
+    var media = MediaQuery.of(context).size;
+    return Scaffold(
+      
+    );
+  }
+}
