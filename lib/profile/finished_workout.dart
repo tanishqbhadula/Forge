761716e05cbe5fb948e34d3c46522cd2209b67fe
TanishqBhadula <tanishqbhadula@gmail.com>
2026-07-06 00:01:41 +0530

    workout finished page

--- lib/profile/finished_workout.dart
+++ lib/profile/finished_workout.dart
@@ -12,7 +12,62 @@ class _FinishedWorkout extends State<FinishedWorkout> {
   Widget build(BuildContext context) {
     var media = MediaQuery.of(context).size;
     return Scaffold(
-      
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const SizedBox(height: 20),
+              Image.asset(
+                "assets/img/complete_workout.png",
+                height: media.width * 0.8,
+                fit: BoxFit.fitHeight,
+              ),
+
+              const SizedBox(height: 20),
+
+              Text(
+                "Congratulations, You Have Finished Your Workout",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+
+              const SizedBox(height: 20),
+
+              Text(
+                "Exercises is king and nutrition is queen. Combine the two and you will have a kingdom",
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.grey, fontSize: 12),
+              ),
+
+              const SizedBox(height: 8),
+
+              Text(
+                "-Jack Lalanne",
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.grey, fontSize: 12),
+              ),
+
+              const Spacer(),
+
+              //  RoundButton(
+              //     title: "Back To Home",
+              //     onPressed: () {
+              //       Navigator.pop(context);
+              //     }),
+
+              //    const SizedBox(
+              //   height: 8,
+              // ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
